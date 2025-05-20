81a2687778ae5ef0140d38766ec2ad7fe9ae5df1
“Manuel <i22pecum@uco.es>
2025-05-20 10:53:54 +0200

    Creando BBDD

--- backup.sql
+++ backup.sql
@@ -17,31 +17,111 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `usuarios`
+-- Table structure for table `mensaje`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
+DROP TABLE IF EXISTS `mensaje`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `email` varchar(100) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `contraseña` varchar(100) NOT NULL,
-  `puntuacion` int(11) DEFAULT 0,
-  PRIMARY KEY (`email`)
+CREATE TABLE `mensaje` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `autor` varchar(100) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `mensaje` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `autor` (`autor`),
+  CONSTRAINT `mensaje_ibfk_1` FOREIGN KEY (`autor`) REFERENCES `usuario` (`correo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuarios`
+-- Dumping data for table `mensaje`
 --
 
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES
-('i22pecum@uco.es','Manuel','1234',0),
-('juan@example.com','Juan','$2a$12$Bl6CqhFjX.1Epv6S4BNzp.ZeuRuG6w6zNYCvuEo/6TiAFlzcWtrH2',0);
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+LOCK TABLES `mensaje` WRITE;
+/*!40000 ALTER TABLE `mensaje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mensaje` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `partida`
+--
+
+DROP TABLE IF EXISTS `partida`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `partida` (
+  `idProblema` int(11) NOT NULL,
+  `correoUsuario` varchar(100) NOT NULL,
+  `duracion` int(11) NOT NULL,
+  `intentos` int(11) NOT NULL,
+  `puntuacion` float NOT NULL,
+  PRIMARY KEY (`idProblema`,`correoUsuario`),
+  KEY `correoUsuario` (`correoUsuario`),
+  CONSTRAINT `partida_ibfk_1` FOREIGN KEY (`correoUsuario`) REFERENCES `usuario` (`correo`),
+  CONSTRAINT `partida_ibfk_2` FOREIGN KEY (`idProblema`) REFERENCES `problema` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `partida`
+--
+
+LOCK TABLES `partida` WRITE;
+/*!40000 ALTER TABLE `partida` DISABLE KEYS */;
+/*!40000 ALTER TABLE `partida` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `problema`
+--
+
+DROP TABLE IF EXISTS `problema`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `problema` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `operacion` varchar(15) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `dificultad` enum('6','8','10','12') DEFAULT NULL,
+  `modo_juego` enum('normal','ninos') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `problema`
+--
+
+LOCK TABLES `problema` WRITE;
+/*!40000 ALTER TABLE `problema` DISABLE KEYS */;
+/*!40000 ALTER TABLE `problema` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `correo` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `puntuacion` float DEFAULT 0,
+  `tema` varchar(20) DEFAULT 'claro',
+  PRIMARY KEY (`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -53,4 +133,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-01 21:49:35
+-- Dump completed on 2025-05-20 10:53:39
